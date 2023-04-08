40cb3fee9d527502d87c63e57bf6e202e630fddc
SpencerShilling <spencershilling@gmail.com>
2023-04-08 13:46:31 -0400

    Core Project

--- Website.py
+++ Website.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('Index.html')
+
+@app.route('/Spencer')
+def Spencer():
     return render_template('SpencerPage.html')
